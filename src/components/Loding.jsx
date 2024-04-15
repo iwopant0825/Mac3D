70edd6ebe7cc8caae6f003192d106886cc4e9057
iwopant0825 <ccodingpy0825@gmail.com>
2024-04-16 02:28:03 +0900

    로딩 구현

--- src/components/Loding.jsx
+++ src/components/Loding.jsx
@@ -1,16 +1,18 @@
 import { PuffLoader } from "react-spinners";
+import styled from "styled-components";
 
 const override = {
-    display: "flex",
-    margin: "0 auto",
-    borderColor: "#E50915",
-    textAlign: "center",
-  };
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  backgroundColor: "#202020"
+};
 
-export default function Loding(){
-    return(
-        <>
-        <PuffLoader color="#36d7b7" cssOverride={override}/>
-        </>
-    )
+export default function Loading() {
+  return (
+    <div style={override}>
+      <PuffLoader color="#36d7b7" />
+    </div>
+  );
 }
